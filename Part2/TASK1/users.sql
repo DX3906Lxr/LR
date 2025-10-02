7478a9674b44a49d30537524d8ec745aa3ba7216
luoxinrui <898534959@qq.com>
2025-10-02 17:41:49 +0800

    add

---
+++ Part2/TASK1/users.sql
@@ -0,0 +1,2 @@
+INSERT INTO mydatabase.users (id, name, email, created_at) VALUES (4, '张三', 'zhangsan@example.com', '2025-10-01 20:18:40');
+INSERT INTO mydatabase.users (id, name, email, created_at) VALUES (5, '李四', 'lisi_new@example.com', '2025-10-01 20:18:40');
